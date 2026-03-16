e247cc1a6cf76e1a8b81e8937efb35ccf8846326
Esteban Gomez <estebangomez@Estebans-MBP.local>
2026-03-16 08:05:46 -0700

    change order

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,8 @@ function App() {
       {/* MapController is now large and centered */}
       <HashRouter>
         <Routes>
-          <Route path="*" element={<MapController />} />
           <Route path="/game/:sessionId" element={<MapController />} />
+          <Route path="*" element={<MapController />} />
         </Routes>
       </HashRouter>
     </div>
